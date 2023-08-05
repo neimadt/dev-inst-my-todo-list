1608c6c2baa4ed0020299b2f316d5dd78e254a4d
neimadt <damtoulet@yahoo.fr>
2023-08-05 10:10:17 +0400

    miscs

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -4,8 +4,11 @@ import '../index.css';
 export const App = () => {
 
     return (
-        <>
+        <main className="w-full h-full pt-8 pb-5 px-5">
             <h1 className="text-5xl text-slate-700 text-center">My Todo List</h1>
-        </>
+            <h3 className="text-2xl font-bold text-slate-700">
+                Tasks
+            </h3>
+        </main>
     );
 };
